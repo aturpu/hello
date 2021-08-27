b1af6e475dc8eb6ffe0df2ccbb48ecbbb9f97b0f
ashritha <aturpu2@huskers.unl.edu>
2021-08-27 19:16:14 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: ashritha reddy turpu
+ *         aturpu2@huskers.unl.edu
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("ASHRITHA REDDY TURPU\n");
+
+  return 0;
+}
